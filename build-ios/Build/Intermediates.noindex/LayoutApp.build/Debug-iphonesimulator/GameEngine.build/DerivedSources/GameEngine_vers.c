21c230430c38388a5d101b86bd8389d07585b196
Max Grebenets <mgrebenets@gmail.com>
2026-06-11 16:03:23 +1000

    Cleanup

--- build-ios/Build/Intermediates.noindex/LayoutApp.build/Debug-iphonesimulator/GameEngine.build/DerivedSources/GameEngine_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char GameEngineVersionString[];
- extern const double GameEngineVersionNumber;
-
- const unsigned char GameEngineVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:GameEngine  PROJECT:LayoutApp-1" "\n";
- const double GameEngineVersionNumber __attribute__ ((used)) = (double)1.;
--- build-ios/Build/Intermediates.noindex/LayoutApp.build/Debug-iphonesimulator/LayoutKit.build/Objects-normal/arm64/LayoutKit_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Applications/Xcode-Rizz.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator27.0.sdk/SDKSettings.json \
-  /Users/max_grebenets/i4napps/kartoteka/layout-app/build-ios/Build/Intermediates.noindex/LayoutApp.build/Debug-iphonesimulator/LayoutKit.build/DerivedSources/LayoutKit_vers.c
